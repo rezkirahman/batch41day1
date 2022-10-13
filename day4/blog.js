bc979d9fb5d22c62d9af6d22bc5fe83bcfdf1370
rezkirahman <rezkirahman1996@gmail.com>
2022-10-14 00:34:52 +0700

    push

--- day4/blog.js
+++ day4/blog.js
@@ -4,7 +4,10 @@ function addBlog(event) {
     event.preventDefault()
 
     let title = document.getElementById("input-title").value
-    let content = document.getElementById("input-content").value
+    let startDate = document.getElementById("input-startdate").value
+    let endDate = document.getElementById("input-enddate").value
+    let description = document.getElementById("input-content").value
+    let tech = document.getElementById("input-tech").value
     let image = document.getElementById("input-blog-image").files[0]
 
     // buat url gambar nantinya tampil
@@ -13,10 +16,13 @@ function addBlog(event) {
 
     let blog = {
         title,
-        content,
+        startDate,
+        endDate,
+        tech,
+        description,
         image,
         postAt: new Date(),
-        author: "Abel Dustin"
+        author: "Rezki Rahman"
     }
 
     dataBlog.push(blog)
@@ -37,21 +43,27 @@ function renderBlog() {
                 <img src="${dataBlog[index].image}">
             </div>
             <div class="blog-content">
-                <div class="btn-group">
-                    <button class="btn-edit">Edit Post</button>
-                    <button class="btn-post">Post Blog</button>
-                </div>
-                <h1>
+                <h3>
                     <a href="blog-detail.html" target="_blank">
                         ${dataBlog[index].title}
                     </a>
-                </h1>
+                </h3>
                 <div class="detail-blog-content">
-                    12 Jul 2021 22:30 WIB | Ichsan Emrald Alamsyah
+                    12 Jul 2021 22:30 WIB | Rezki Rahman
                 </div>
                 <p>
                     ${dataBlog[index].content}
                 </p>
+                <div style="text-align: left; font-size: 20px; " >
+                    <a href="" style="color: black;"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="" style="color: black;"><i class="fa-brands fa-facebook" ></i></a>
+                    <a href="" style="color: black;"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="" style="color: black;"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+            </div>
+            <div class="btn-group">
+                    <button class="btn-detail" style="margin-right: 4px;">Edit Post</button>
+                    <button class="btn-detail" style="margin-left: 4px;">Post Blog</button>
             </div>
         </div>
         `
